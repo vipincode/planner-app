f8cbf88becb6bfb4f43da73c377290cfa4451505
Vipin Singh <svipin174@gmail.com>
2022-03-11 09:19:44 +0530

    Add navigate propert move between screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 
-const HomeScreen = ({navigation}: any) => {
+const HomeScreen = ({navigation}: NativeStackHeaderProps) => {
 
   useEffect(() => {
     console.log('Rendering Home Screen');
@@ -10,7 +11,7 @@ const HomeScreen = ({navigation}: any) => {
   return (
     <View>
       <Text>I am Home Screen</Text>
-      <Button title="Go to Planner" onPress={() => navigation.push('Planner')} />
+      <Button title="Go to Planner" onPress={() => navigation.navigate('Planner')} />
     </View>
   );
 };
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import {View, Text, Button} from 'react-native';
+import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 
-const PlannerScreen = ({navigation}:any) => {
+const PlannerScreen = ({navigation}: NativeStackHeaderProps) => {
   
   useEffect(() => {
     console.log('Rendering Planner Screen');
@@ -10,7 +11,7 @@ const PlannerScreen = ({navigation}:any) => {
   return (
     <View>
       <Text>Welcome planner</Text>
-      <Button title="Go to Home" onPress={() => navigation.push("Home")} />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
     </View>
   );
 };
